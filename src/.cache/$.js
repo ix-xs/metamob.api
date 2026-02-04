ec722b0831e4c5144a6a24c0778bad799ca6447a
ix-xs <poplomo@outlook.fr>
2026-02-04 14:34:09 +0100

    Create $.js

---
+++ src/.cache/$.js
@@ -0,0 +1,7 @@
+module.exports = {
+	gameVersions: require("./gameVersions.json"),
+	monsters: require("./monsters.json"),
+	monsterTypes: require("./monsterTypes.json"),
+	questTemplates: require("./questTemplates.json"),
+	servers: require("./servers.json"),
+};
