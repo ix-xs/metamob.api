77e1b343173c5ad5a41640d5ddaaa4aab717bc78
Ix-xs <poplomo@outlook.fr>
2026-07-18 18:43:48 +0200

    Create cache module to load JSON data

---
+++ src/cache/$.js
@@ -0,0 +1,10 @@
+module.exports = {
+  game_versions: require("./game_versions.json"),
+  monster_types: require("./monster_types.json"),
+  monsters: require("./monsters.json"),
+  quest_templates: require("./quest_templates.json"),
+  quest_types: require("./quest_types.json"),
+  servers: require("./servers.json"),
+  subzones: require("./subzones.json"),
+  zones: require("./zones.json"),
+};
